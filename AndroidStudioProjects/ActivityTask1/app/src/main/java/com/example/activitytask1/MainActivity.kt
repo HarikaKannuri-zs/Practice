9f00ce92655a9f3e87dd1c28994e1b450069e112
Raramuri <21A91A05E3@aec.edu.in>
2024-07-03 23:09:45 +0530

    ref #3 Data Sharing

---
+++ AndroidStudioProjects/ActivityTask1/app/src/main/java/com/example/activitytask1/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.activitytask1
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    val userList = mutableListOf<User>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+
+        }
+
+
+        }
+    }
+
+data class User(val userName : String , val userPhone : String , )
+
+
+
+
+
+
+
+
